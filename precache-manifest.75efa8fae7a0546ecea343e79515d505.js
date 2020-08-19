02bbe25f59516c4b2dd01e900b241a8384cfa933
Ravikesh Yadav <ravikesh@netzary.com>
2020-08-20 02:30:33 +0530

    Updates

---
+++ precache-manifest.75efa8fae7a0546ecea343e79515d505.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8774815afbfbb33b8cfbe691a67d8a51",
+    "url": "/whatsapp/index.html"
+  },
+  {
+    "revision": "9c08ce59415baf515af3",
+    "url": "/whatsapp/static/css/main.865afe18.chunk.css"
+  },
+  {
+    "revision": "0cf9b196dfe6d83c8195",
+    "url": "/whatsapp/static/js/2.a02601ba.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/whatsapp/static/js/2.a02601ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c08ce59415baf515af3",
+    "url": "/whatsapp/static/js/main.98c5b94b.chunk.js"
+  },
+  {
+    "revision": "6cb871817611e35f2201",
+    "url": "/whatsapp/static/js/runtime-main.d38291f1.js"
+  }
+]);
